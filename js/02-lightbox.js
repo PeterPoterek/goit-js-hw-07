638cb5dd5adfe5f2551dc57827a8320e0d963eae
Piotr Poterek <peterpoterek0@gmail.com>
2023-11-02 18:33:08 +0100

    Task 2 - part 1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+  });
+  console.log(lightbox);
+});
+
+const galleryItemArr = [];
+
+galleryItems.forEach((item) => {
+  const galleryItem = document.createElement("li");
+  galleryItem.setAttribute("class", "gallery__item ");
+
+  const image = document.createElement("img");
+  image.setAttribute("class", "gallery__image");
+  image.src = item.preview;
+  image.alt = item.description;
+
+  const link = document.createElement("a");
+  link.setAttribute("href", item.original);
+  link.setAttribute("class", "gallery__link");
+
+  link.append(image);
+
+  galleryItem.append(link);
+  galleryItemArr.push(galleryItem);
+});
+
+gallery.append(...galleryItemArr);
+
+gallery.addEventListener("click", (e) => {
+  if (e.target.tagName === "IMG") {
+    lightbox.open();
+  }
+});
