ecbc9438e2b704adb000302ebaf7cf79bcfbd28a
Piotr Poterek <peterpoterek0@gmail.com>
2023-11-02 17:25:46 +0100

    task 1 - completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+  const galleryItem = document.createElement("li");
+  galleryItem.setAttribute("class", "gallery__item ");
+
+  const image = document.createElement("img");
+  image.setAttribute("class", "gallery__image");
+  image.src = item.preview;
+  image.alt = item.description;
+
+  galleryItem.append(image);
+  gallery.append(galleryItem);
+
+  galleryItem.addEventListener("click", () => {
+    const instance = basicLightbox.create(`
+    <img src="${item.original}" alt="${item.description}">
+  `);
+    instance.show();
+  });
+});
