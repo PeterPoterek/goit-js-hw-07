43c571297e7f046c2a9648405f7ae960581d79e8
Piotr Poterek <peterpoterek0@gmail.com>
2023-11-02 19:41:00 +0100

    Task 1 - refactor

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,36 +3,45 @@ import { galleryItems } from "./gallery-items.js";
 const gallery = document.querySelector(".gallery");
 const galleryItemArr = [];
 
-galleryItems.forEach((item) => {
-  const galleryItem = document.createElement("li");
-  galleryItem.setAttribute("class", "gallery__item ");
+const handleImageRender = () => {
+  galleryItems.forEach((item) => {
+    const galleryItem = document.createElement("li");
+    galleryItem.setAttribute("class", "gallery__item ");
 
-  const image = document.createElement("img");
-  image.setAttribute("class", "gallery__image");
-  image.setAttribute("data-source", item.original);
-  image.src = item.preview;
-  image.alt = item.description;
+    const image = document.createElement("img");
+    image.setAttribute("class", "gallery__image");
+    image.setAttribute("data-source", item.original);
+    image.src = item.preview;
+    image.alt = item.description;
 
-  const link = document.createElement("a");
-  link.setAttribute("href", item.original);
-  link.setAttribute("class", "gallery__link");
+    const link = document.createElement("a");
+    link.setAttribute("href", item.original);
+    link.setAttribute("class", "gallery__link");
 
-  link.append(image);
+    link.append(image);
 
-  link.addEventListener("click", (e) => {
-    e.preventDefault();
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+    });
+
+    galleryItem.append(link);
+    galleryItemArr.push(galleryItem);
+
+    gallery.append(...galleryItemArr);
   });
+};
+const handleImageClick = () => {
+  gallery.addEventListener("click", (e) => {
+    const img = e.target.attributes[1].nodeValue;
+    const description = e.target.attributes[3].nodeValue;
 
-  galleryItem.append(link);
-  galleryItemArr.push(galleryItem);
-
-  galleryItem.addEventListener("click", () => {
     const instance = basicLightbox.create(`
-    <img src="${item.original}" alt="${item.description}">
+    <img src="${img}" alt="${description}">
   `);
 
     instance.show();
   });
-});
+};
 
-gallery.append(...galleryItemArr);
+handleImageRender();
+handleImageClick();
